46b2475a6d10fddf3d2efc2ed077461f33a43928
rajeshsharma-ai <65742796+rajeshsharma-ai@users.noreply.github.com>
2021-06-25 16:06:56 -0700

    Create od.py
    file for testing python bindings to Intel OIDN

---
+++ od.py
@@ -0,0 +1,42 @@
+import imageio
+import pyoidn
+import numpy as np
+import sys
+import glob
+import os
+
+def denoiseDir(idir, odir):
+    files = glob.glob(idir+'/*.png')
+    for f in files:
+        # /idir/foo.png --> /odir/dfoo.png
+        denoiseFile(f, odir+'/d'+os.path.basename(f))
+
+def denoiseFile(infile, outfile):
+    # read the image file (noisy)
+    im = imageio.imread(infile)
+
+    # scale and convert to float32
+    im = (im/255.0).astype(np.float32)
+
+    #### run the denoiser ###
+    im = pyoidn.run_oidn(im, None, None)
+    #im = pyoidn.run_oidn(im)
+
+    # scale and convert to uint8
+    im = (im*255).round().astype(np.uint8)
+
+    # write out the result
+    imageio.imwrite(outfile, im)
+
+def main(argv):
+    ifilename = argv[0]
+    ofilename = argv[1]
+    denoiseFile(ifilename, ofilename)
+
+if __name__ == "__main__":
+    args = sys.argv
+    #help(pyoidn.run_oidn)
+    if len(args) != 3:
+        print("Usage: ", args[0], " inputfile outputfile")
+    else:
+        main(sys.argv[1:])
